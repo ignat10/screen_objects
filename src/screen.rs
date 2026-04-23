dd93107b735470e2e298451189079b7174a33a98
Ignat10 <Ignat.shikh@gmail.com>
2026-04-23 23:24:14 +0200

    fix mutex problem

--- src/screen.rs
+++ src/screen.rs
@@ -2,24 +2,32 @@ use std::sync::{MappedMutexGuard, Mutex, MutexGuard};
 
 
 
-use image;
+use image::{RgbImage, RgbaImage, DynamicImage};
 
 use crate::adb;
 
 
-static SCREEN: Mutex<Option<image::RgbImage>> =  Mutex::new(None);
+static SCREEN: Mutex<Option<RgbImage>> =  Mutex::new(None);
 
 
 
-pub(super) fn get() -> MappedMutexGuard<'static, image::RgbImage> {
-    let guard = SCREEN.lock().unwrap();
+pub(super) fn get() -> MappedMutexGuard<'static, RgbImage> {
+    let mut guard = SCREEN.lock().unwrap();
     
-    if guard.as_ref().is_none() {
-        drop(guard);
-        set();
-    }
+    if guard.is_none() {
+        let (w, h) = adb::dimensions();
+        let output_bytes = adb::screencap();
 
-    let guard = SCREEN.lock().unwrap();
+        let rgba_img = RgbaImage::from_raw(
+            w,
+            h,
+            output_bytes,
+        ).expect("Failed to create RGB array from screencap bytes");
+
+        let rgb_img = DynamicImage::from(rgba_img).to_rgb8();
+
+        *guard = Some(rgb_img);
+    }
     
     MutexGuard::map(guard, |opt| opt.as_mut().unwrap())
 }
@@ -27,22 +35,4 @@ pub(super) fn get() -> MappedMutexGuard<'static, image::RgbImage> {
 
 pub(super) fn reset() {
     *SCREEN.lock().unwrap() = None;
-}
-
-
-fn set() {
-    let mut guard = SCREEN.lock().unwrap();
-
-    let (w, h) = adb::dimensions();
-    let output_bytes = adb::screencap();
-
-    let rgba_img = image::RgbaImage::from_raw(
-        w,
-        h,
-        output_bytes,
-    ).expect("Failed to create RGB array from screencap bytes");
-
-    let rgb_img = image::DynamicImage::from(rgba_img).to_rgb8();
-
-    *guard = Some(rgb_img);
 }
