2dbbfc9030ca6240ba9295f607ef158664b864b6
ignat10 <ignat.shikh@gmail.com>
2026-06-22 23:41:15 +0200

    Fix lock drop order in set_tolerance function

--- src/utils.rs
+++ src/utils.rs
@@ -36,7 +36,7 @@ pub(super) fn set_tolerance(key: &str, tolerance: u8) -> PyResult<()> {
     let mut lock = get_lock()?;
     let t = lock.get_mut(key).unwrap();
     t.1 = t.1.max(tolerance);
-    drop(t);
+    drop(lock);
     save_data()?;
     Ok(())
 }
