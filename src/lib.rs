943d9568c1e5385450d3a5418f8baeb04d7137d3
ignat10 <ignat.shikh@gmail.com>
2026-06-29 17:47:45 +0200

    better error message

--- src/lib.rs
+++ src/lib.rs
@@ -136,7 +136,7 @@ fn get_objects(objects_dir: PathBuf, regions_dir: Option<PathBuf>) -> PyResult<H
                 regions
                     .get(&k)
                     .copied()
-                    .ok_or_else(|| PyValueError::new_err("Failed to retrieve data from regions"))
+                    .ok_or_else(|| PyValueError::new_err(format!("There is not region called {}. existing regions: {:?}", k, regions.unwrap().keys().collect::<Vec<_>>())))
             })
             .transpose()?;
 
