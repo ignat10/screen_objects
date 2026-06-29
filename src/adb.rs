f6efea6329eaace6b44160a31e8aa713bc9dd100
ignat10 <ignat.shikh@gmail.com>
2026-06-29 16:45:42 +0200

    Improve error handling for device serial retrieval

--- src/adb.rs
+++ src/adb.rs
@@ -127,7 +127,7 @@ fn connect(port: &str) -> PyResult<bool> {
 fn device_action(args: &[&str]) -> PyResult<Output> {
     let serial = DEVICE_SERIAL
         .get()
-        .expect("serial not set. call device_config() before using actions.");
+        .map_err(|_| PyValueError::new_err("serial not set. call device_config() before using actions."))?;
     let args = [&["-s", serial], args].concat();
     run(&args)
 }
